ed52abe550582249bfa3ae9312d9e3966afa5d61
samer tawil <eng.samertawil@gmail.com>
2024-12-26 11:03:32 +0200

    updates package

---
+++ lang/en/ucModule.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'name' => 'Full name',
+    'user_name' => 'User name',
+    'user_type' => 'User type',
+    'user_activation' => 'User activation',
+    'mobile'=>'Mobile',
+    'actions'=>'Actions',
+    'record'=>'Record',
+    'search'=>'Search',
+    'all'=>'All',
+    
+];
--- resources/views/partials/_loading.blade.php
+++
@@ -1,5 +0,0 @@
-
-<div wire:loading   >
-    <img src="{{ URL::asset('assets/img/loader.svg') }}"  
-        alt="Loader">
-</div>
